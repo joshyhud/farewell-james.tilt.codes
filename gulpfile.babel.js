aa622bc50b45ff891c4c6f7e2fba7f90ec89d0cc
joshyhud <joshyhud>
2020-08-27 15:00:05 +0100

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,296 @@
+'use strict';
+
+//
+// Packages
+//
+
+const gulp = require('gulp');
+const del = require('del');
+const svgsprite = require('gulp-svg-sprite');
+const imagemin = require('gulp-imagemin');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sasslint = require('gulp-sass-lint');
+const ext = require('gulp-ext-replace');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const mustache = require("gulp-mustache");
+const favicon = require('gulp-base64-favicon');
+const browsersync = require('browser-sync').create();
+
+//
+// Processes
+//
+
+// Clean
+function clean() {
+  return del('./dist');
+}
+
+//
+// JS
+//
+
+function jslint() {
+  return gulp
+    .src([
+      './src/js/*/js',
+      './gulpfile.babel.js'
+    ], { allowEmpty: true })
+    .pipe(jshint()) .pipe(jshint.reporter(stylish)) .pipe(browsersync.stream()); }
+
+function jslazyloadmodules() {
+  return gulp
+    .src([
+      './node_modules/loadjs/dist/loadjs.min.js'
+    ], { allowEmpty: true })
+    .pipe(ext('.mustache'))
+    .pipe(gulp.dest('./src/mustache/partials/global/'))
+    .pipe(browsersync.stream()); }
+
+function jscritical() {
+  return gulp
+    .src('./src/js/critical.js', { allowEmpty: true })
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(uglify({
+      mangle: true,
+      compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: false
+      }}
+    ))
+    .pipe(concat('js.js'))
+    .pipe(ext('.mustache'))
+    .pipe(gulp.dest('./src/mustache/partials/global/'))
+    .pipe(browsersync.stream());
+}
+
+function jsnoncritical() {
+  return gulp
+    .src([
+      './src/js/main.js',
+      './node_modules/fg-loadcss/dist/loadCSS.js',
+      './node_modules/vanilla-lazyload/dist/lazyload.js'
+    ], { allowEmpty: true })
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+        overrides: [{
+          test: './node_modules/**',
+          sourceType: 'script'
+        }]
+      })
+    )
+    .pipe(uglify({
+      mangle: true,
+      compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: false
+      }}
+    ))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(browsersync.stream());
+}
+
+
+//
+// CSS
+//
+
+function csslint() {
+  return gulp
+    .src('./src/scss/*.scss', { allowEmpty: true })
+    .pipe(sasslint({'configFile': '.sass-lint.yml'}))
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError())
+    .pipe(browsersync.stream()); }
+
+function csscritical() {
+  return gulp
+    .src('./src/scss/critical.scss', { allowEmpty: true })
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(concat('css.scss'))
+    .pipe(ext('.mustache'))
+    .pipe(gulp.dest('./src/mustache/partials/global/'))
+    .pipe(browsersync.stream());
+}
+
+function cssnoncritical() {
+  return gulp
+    .src('./src/scss/main.scss', { allowEmpty: true })
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browsersync.stream());
+}
+
+
+//
+// Assets
+//
+
+function sprite() {
+  return gulp
+    .src('./src/sprite/**/**/*.svg', { allowEmpty: true })
+    .pipe(
+      svgsprite({
+        shape: {
+          spacing: {
+            padding: 5
+          }
+        },
+        mode: {
+          symbol: true
+        },
+        svg: {
+          xmlDeclaration: false,
+          doctypeDeclaration: false,
+          namespaceIDs: false,
+          namespaceClassnames: false
+        }
+      })
+    )
+    .pipe(concat('sprite.mustache'))
+    .pipe(gulp.dest('./src/mustache/partials/global/'))
+    .pipe(browsersync.stream());
+}
+
+function images() {
+  return gulp
+    .src('./src/img/**/**/*', { allowEmpty: true })
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest('./dist/img/'))
+    .pipe(browsersync.stream());
+}
+
+
+//
+// HTML
+//
+
+// HTML
+function html() {
+  return gulp
+    .src('./src/mustache/*.mustache', { allowEmpty: true })
+    .pipe(mustache())
+    .pipe(ext('.html'))
+    .pipe(favicon())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browsersync.stream());
+}
+
+
+//
+// Testing environment
+//
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./dist"
+    },
+    port: 3000
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+
+//
+// Watch and build scripts
+//
+
+const watch =
+  gulp.series(
+    clean,
+    gulp.parallel(
+      sprite,
+      images,
+      jslint,
+      csslint
+    ),
+    gulp.parallel(
+      csscritical,
+      cssnoncritical,
+      jscritical,
+      jsnoncritical
+    ),
+    jslazyloadmodules,
+    html,
+    browserSync,
+    watchFiles
+  );
+
+const csswatch =
+  gulp.series(
+    csslint,
+    csscritical,
+    cssnoncritical,
+    html
+  );
+
+const jswatch =
+  gulp.series(
+    jslint,
+    jscritical,
+    jsnoncritical,
+    html
+  );
+
+function watchFiles() {
+  gulp.watch('./src/scss/**/**/*.scss', csswatch);
+  gulp.watch('./src/js/**/*.js', jswatch);
+  gulp.watch('./src/sprite/**/**/*.svg', sprite);
+  gulp.watch('./src/mustache/**/**/**/*.mustache', html);
+  gulp.watch('./src/img/**/**/*', images);
+}
+
+
+//
+// Export tasks
+//
+
+exports.default = watch;
